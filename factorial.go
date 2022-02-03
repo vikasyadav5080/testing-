cae323e5ff7b2cadc3e5b9ab189455d76028d1c9
VIKAS yadav <84800949+vikasyadav5080@users.noreply.github.com>
2022-02-03 09:28:27 +0530

    Create factorial.go

---
+++ factorial.go
@@ -0,0 +1,15 @@
+package main
+
+// import "fmt"
+
+func Factorial(input int) int{
+	fact := 1
+	for i:=input ; i>0 ; i-- {
+		fact *= 1
+	}
+	return fact
+}
+
+func main() {
+	// fmt.Println("The Factorial is something!")
+}
